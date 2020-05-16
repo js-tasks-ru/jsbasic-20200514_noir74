6e868be8b4f16d5aea67a4cfe9c8065263eb0185
Roman Narinyan <noir74@mail.ru>
2020-05-16 22:00:36 +0300

    factorial (code style)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,8 +4,11 @@
  * @returns {number}
  */
 function factorial(n) {
-    let returnValue = 1;
-    if (n > 2)
-        while (n > 0) returnValue *= (n--);
-    return returnValue;
+    let factorialValue = 1;
+
+    if (n > 2) {
+        while (n > 0) factorialValue *= (n--);
+    }
+        
+    return factorialValue;
 }
