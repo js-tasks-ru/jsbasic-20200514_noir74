3b805fbce4a3c170b6b9ff016ddc9e804e4db63b
Roman Narinyan <noir74@mail.ru>
2020-05-24 23:41:50 +0300

    summary salaries

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,7 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-    return (str.length <= maxlength) ? str : str.slice(0,maxlength-1) + '…';
-  }
+  return (str.length <= maxlength) ? str : str.slice(0, maxlength - 1) + '…';
+}
+
+console.log(truncate('qqqqq', 3));
--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,10 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+    let rezultValue = 0;
+    for (key in salaries) {
+        if (typeof (salaries[key]) == 'number')
+            rezultValue += salaries[key];
+    }
+    return rezultValue;
 }
